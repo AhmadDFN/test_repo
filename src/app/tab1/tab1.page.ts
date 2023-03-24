9b1169db881e04fd47e7350c4ea2b1e4fd0dd839
dasaraul <acmadauliai@example.com>
2023-03-24 15:43:18 +0700

    first Commit

---
+++ src/app/tab1/tab1.page.ts
@@ -0,0 +1,42 @@
+import { HttpClient } from '@angular/common/http';
+import { Component } from '@angular/core';
+import { GlobalService } from '../services/global.service';
+
+
+@Component({
+  selector: 'app-tab1',
+  templateUrl: 'tab1.page.html',
+  styleUrls: ['tab1.page.scss']
+})
+export class Tab1Page {
+
+  menus: any
+  user: any = {}
+
+  constructor(
+    public http: HttpClient,
+    public gb: GlobalService
+  ) {
+
+  }
+
+  ionViewDidEnter() {
+    this.getMenu()
+    this.getProfile()
+  }
+
+  getProfile() {
+    let data = localStorage.getItem("login")
+    this.user = data ? JSON.parse(data) : null
+  }
+
+  getMenu() {
+    this.http.get(this.gb.API_URL + "menu_fav").toPromise()
+      .then(res => {
+        this.menus = res
+        console.log(res)
+      })
+  }
+
+
+}
